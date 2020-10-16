4aabe00b98bd26e78610d03f81f5de3176a212ba
Nuria Romero <anromercas@gmail.com>
2020-10-16 11:42:00 +0200

    prueba1

---
+++ precache-manifest.56a9afb412f6290e3a9327d7a11b2a07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e594508fe6da595bfb5d139c14ca8ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63ad0e5d5c431667085c",
+    "url": "/static/js/2.857e3407.chunk.js"
+  },
+  {
+    "revision": "b40ab9903c85e214796e20bc82c030a7",
+    "url": "/static/js/2.857e3407.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68fbe262d1473b01ee42",
+    "url": "/static/js/main.307f3340.chunk.js"
+  },
+  {
+    "revision": "fd1e764c12408bbbbfc1",
+    "url": "/static/js/runtime-main.f859b388.js"
+  }
+]);
